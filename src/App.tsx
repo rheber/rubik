667908dc1df2adacbcc2dc1dda4b8fb66652d9db
rheber <rheber@rediffmail.com>
2023-04-25 14:36:03 +1000

    tree up to ocll

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,62 @@
 import type { Component } from 'solid-js';
 
-import logo from './logo.svg';
 import styles from './App.module.css';
 
 const App: Component = () => {
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
+    <div>
+      <div class={styles.row}>
+        <div class={styles.column}>
+          <div class={styles.header}>
+            3x3x3
+          </div>
+          <div class={styles.row}>
+            <div class={styles.column}>
+              Beginner
+            </div>
+            <div class={styles.column}>
+              <div class={styles.header}>
+                CFOP
+              </div>
+              <div class={styles.column}>
+                Cross
+              </div>
+              <div class={styles.column}>
+                F2L
+              </div>
+              <div class={styles.column}>
+                <div class={styles.header}>
+                  OLL
+                </div>
+                <div class={styles.row}>
+                  <div class={styles.column}>
+                    1L
+                  </div>
+                  <div class={styles.column}>
+                    <div class={styles.header}>
+                      2L
+                    </div>
+                    <div class={styles.column}>
+                      EOLL
+                    </div>
+                    <div class={styles.column}>
+                      OCLL
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class={styles.column}>
+                PLL
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class={styles.column}>
+          <div class={styles.header}>
+            4x4x4
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
